fb52021380996841cc08dd79c6b98eaf7ff234cd
Potetrompe <46726271+Potetrompe@users.noreply.github.com>
2019-01-31 23:32:20 +0100

    added feed & deletebtn

--- js/main.js
+++ js/main.js
@@ -1,7 +1,51 @@
 var feed = document.getElementById("feed");
+var btnDelete = document.getElementById("btnDelete");
 
 //* Globals
-var tdArr = [];
+var trArr = [];
+//trArr[i].firstElementChild.innerHTML = "";\
+var trActive = true;
+
+function newIndex(text, auth, bg, col){
+    var tr = document.createElement("tr");
+    var tdText = document.createElement("td");
+    var tdAuth = document.createElement("td");
+
+    text = String(text);
+    auth = String(auth);
+
+    tdText.innerHTML = text;
+    tdAuth.innerHTML = auth;
+    
+    tr.appendChild(tdText);
+    tr.appendChild(tdAuth);
+
+    if(col != "" && bg != ""){
+        tr.style.background = "#0ff";
+        tr.style.color = "#000";
+    }
+    trArr.push(tr);
+    for (let i = 0; i < trArr.length; i++) {
+
+        trArr[i].onclick = function(){
+            this.classList.toggle("trActive");
+            
+            if(trActive){
+                btnDelete.style.visibility = "visible";
+                trActive = false;
+            }else{
+                btnDelete.style.visibility = "hidden";
+                trActive = true;
+            }
+            //console.log(this.classList);
+        }
+        
+    }
+
+    feed.firstElementChild.firstElementChild.appendChild(tr);
+}
+
+newIndex("text", "auth", "s");
 
 
-var td = new td
+console.log(trArr);
